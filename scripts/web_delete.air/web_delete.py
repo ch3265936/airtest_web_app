2163e9c35a469e8a6a326fdb1331074ac8c8a543
chi <939649080@qq.com>
2019-10-21 14:05:15 +0800

    修改web 自動化業務

--- scripts/web_delete.air/web_delete.py
+++ scripts/web_delete.air/web_delete.py
@@ -2,22 +2,16 @@ __author__ = "chihai"
 
 from airtest.core.api import *
 
-from utils import  *
+
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from airtest_selenium.proxy import WebChrome
-def delete(key):
-    driver.find_element_by_xpath("//input[@type='text']").send_keys(key)
-    sleep(3)
-    driver.find_element_by_xpath("//*[@id=\"contentlist\"]/li/span").click()
-    sleep(10)
-    Logging("获取数据")
-    driver.find_element_by_name("getdata").click()
-    sleep(10)
-    Logging("清空数据")
-    driver.find_element_by_name("cleardata").click()
-    sleep(10)
 driver = WebChrome()
 driver.implicitly_wait(20)
+
+
+from utils import  *
+
 driver.get("http://tech-support.upltv.com:82/index")
 
 driver.find_element_by_xpath("//a[@href='/auth/login']").click()
@@ -29,10 +23,28 @@ sleep(5)
 driver.find_element_by_xpath("//*[@id=\"bs-example-navbar-collapse-1\"]/ul/li/a/span").click()
 driver.find_element_by_xpath("//a[@href='/report/analysis']").click()
 
-#DHM
-delete('600164')
-#TBC
-delete('600141')
+driver.find_element_by_xpath("//input[@type='text']").send_keys('600164')
+sleep(3)
+driver.find_element_by_xpath("//*[@id=\"contentlist\"]/li/span").click()
+sleep(10)
+Logging("获取数据")
+driver.find_element_by_name("getdata").click()
+sleep(10)
+driver.execute_script('window.scrollTo(0,400)')
+sleep(5)
+driver.snapshot()
+driver.execute_script('window.scrollTo(0,800)')
+sleep(5)
+
+driver.snapshot()
+Logging("清空数据")
+driver.execute_script('window.scrollTo(0,0)')
+sleep(5)
+driver.snapshot()
+driver.find_element_by_name("cleardata").click()
+sleep(5)
+driver.snapshot()
+
 
 
 
